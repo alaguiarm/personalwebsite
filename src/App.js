b9cd36a941eb97e0f7c4b894bcb1a29c71bb6f3d
Andrea Aguiar <andreaaguiar@Andreas-MacBook-Pro.local>
2023-12-12 18:14:56 -0600

    Revert "Change src files"
    This reverts commit 114fc08bf70779a3601a6767ab665fa0c443ebe4.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { useEffect } from "react";
-import AllRoutes from "./router/AllRoutes";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import "photoswipe/dist/photoswipe.css";
-import { ToastContainer } from "react-toastify";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  useEffect(() => {
-    AOS.init();
-  }, []);
+function App() {
   return (
-    <div className="tokyo_tm_all_wrap">
-      <AllRoutes />
-      <ToastContainer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
